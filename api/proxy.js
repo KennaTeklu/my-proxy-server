2e1985934a44ae85cca46a1456b6b6e33b4ad3d1
KennaTeklu <140216776+KennaTeklu@users.noreply.github.com>
2026-02-22 20:22:44 -0700

    Update proxy URL to the actual Apps Script URL

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async (req, res) => {
-  const url = 'YOUR_APPS_SCRIPT_URL' + (req.url || '');
+  const url = 'https://script.google.com/macros/s/AKfycbwUFXT4bp2yO7HbGsnmMm7OavUCq3vOXhzoL3MDgJxdDmqcGwoZoVnrO2tJRg0sMA05gw/exec' + (req.url || '');
   const response = await fetch(url, {
     method: req.method,
     headers: req.headers,
